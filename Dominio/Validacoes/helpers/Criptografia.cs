9b3f41b1211850cfe8d34329ca259b70b84acfa9
Aleksei da Silva <alekseidasilva@hotmail.com>
2020-06-18 03:07:07 +0100

    Mudança para DDD

---
+++ Dominio/Validacoes/helpers/Criptografia.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Validacoes.helpers
+{
+   public class Criptografia
+    {
+
+        private const string AesIV256 = @"AlekseidaSilva15";
+        private const string AesKey256 = @"AlekseiJustinoMateusDondodaSilva";
+        public static string Encriptar(string text)
+        {
+            // AesCryptoServiceProvider
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            aes.BlockSize = 128;
+            aes.KeySize = 256;
+            aes.IV = Encoding.UTF8.GetBytes(AesIV256);
+            aes.Key = Encoding.UTF8.GetBytes(AesKey256);
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            // Convert string to byte array
+            byte[] src = Encoding.Unicode.GetBytes(text);
+
+            // encryption
+            using (ICryptoTransform encrypt = aes.CreateEncryptor())
+            {
+                byte[] dest = encrypt.TransformFinalBlock(src, 0, src.Length);
+
+                // Convert byte array to Base64 strings
+                return Convert.ToBase64String(dest);
+            }
+        }
+        public static string Desincriptar(string text)
+        {
+            // AesCryptoServiceProvider
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            aes.BlockSize = 128;
+            aes.KeySize = 256;
+            aes.IV = Encoding.UTF8.GetBytes(AesIV256);
+            aes.Key = Encoding.UTF8.GetBytes(AesKey256);
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            // Convert Base64 strings to byte array
+            byte[] src = System.Convert.FromBase64String(text);
+
+            // decryption
+            using (ICryptoTransform decrypt = aes.CreateDecryptor())
+            {
+                byte[] dest = decrypt.TransformFinalBlock(src, 0, src.Length);
+                return Encoding.Unicode.GetString(dest);
+            }
+        }
+    }
+}
---
+++ Dominio/Validacoes/helpers/EnumeradorEstado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Validacoes.helpers
+{
+   public enum EnumeradorEstado
+    {
+        Pendente=0,
+        Ativo,
+        Bloqueado,
+        Fechado,
+        Concluido,
+        Eliminado
+    }
+}
