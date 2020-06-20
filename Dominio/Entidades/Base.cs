4694f5f33c30fe2ab4389b72aac883808efabb77
Aleksei da Silva <alekseidasilva@hotmail.com>
2020-06-20 22:43:55 +0100

    instalação de Pacotes

--- Dominio/Entidades/Base.cs
+++ Dominio/Entidades/Base.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dominio.Entidades
 {
-   public class Base
+    public class Base
     {
         public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataCadastro { get; set; }
     }
 }
