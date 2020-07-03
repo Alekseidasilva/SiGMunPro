66f38d1300677482ffed85ac1d32133c552a6de4
Alekseidasilva <44441672+Alekseidasilva@users.noreply.github.com>
2020-07-03 16:15:19 +0100

    Nem Sei

--- Dominio/Entidades/UsuarioVM.cs
+++ Dominio/Entidades/UsuarioVM.cs
@@ -8,11 +8,6 @@ namespace Dominio.Entidades
 {
    public class UsuarioVM
     {
-        public string Email { get; set; }
-         public string Senha { get; set; }
-         public bool LembrarMe { get; set; }
-         public string ReturnUrl { get; set; }
-
 
     }
 }
---
+++ EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+   public class ApplicationDbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
