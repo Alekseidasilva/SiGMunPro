1d29f634e8c7eb2ed3768072c3e02517c733adc2
Aleksei da Silva <alekseidasilva@hotmail.com>
2020-07-06 15:46:44 +0100

    Add Startup classe

---
+++ MVC/Startup.cs
@@ -0,0 +1,17 @@
+﻿
+using Owin;
+
+
+
+namespace MVC
+{
+    //[assembly:OwinstartupAttribute(typeof(MVC.Startup)) ]
+    public partial class Startup
+    {
+        //public void Configuration(IAppBuilder app)
+        //{
+        //    ConfigureAuth(app);
+        //}
+        
+    }
+}
