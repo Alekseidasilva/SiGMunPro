fe0707e403d04fc06789c46443dd02fc16981109
Aleksei da Silva <alekseidasilva@hotmail.com>
2020-07-07 11:41:22 +0100

    Antes dda LIMPEZA GERAL

---
+++ Dominio/Contexto/Context.cs
@@ -0,0 +1,15 @@
+﻿using Dominio.Entidades.PerfilUsuario;
+using Dominio.Entidades.Usuario;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Dominio.Contexto
+{
+    public class Context : IdentityDbContext<Usuario, Perfil, int, UsuarioLogin, PerfilUsuario, Afirmacoes>
+    {
+        public Context()
+            : base("DefaultConnection")
+        {
+        }
+    
+    }
+}
---
+++ Dominio/Entidades/Usuario/Usuario.cs
@@ -0,0 +1,10 @@
+﻿using Dominio.Entidades.PerfilUsuario;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Dominio.Entidades.Usuario
+{
+    public class Usuario:IdentityUser<int,UsuarioLogin,PerfilUsuario.PerfilUsuario,Afirmacoes>
+    {
+    }
+}
+   
