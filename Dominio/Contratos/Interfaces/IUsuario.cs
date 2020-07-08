468ad43131831413106e8e481da881bad590f028
Alekseidasilva <44441672+Alekseidasilva@users.noreply.github.com>
2020-07-08 08:41:45 +0100

    Add Usuario

--- Dominio/Contratos/Interfaces/IUsuario.cs
+++ Dominio/Contratos/Interfaces/IUsuario.cs
@@ -4,6 +4,8 @@
    {
        IUsuario BuscarPorEmail(string email);
        IUsuario BuscarPorNome(string nome);
+       IUsuario Login(string email, string senha);
+       void alterarSenha(string senhaAntiga, string senhanova, string confirmarSenha);
 
    }
 }
