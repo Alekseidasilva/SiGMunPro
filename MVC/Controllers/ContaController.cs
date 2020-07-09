c83a4df3e82979d6880481ee3c42819fd6052183
Alekseidasilva <44441672+Alekseidasilva@users.noreply.github.com>
2020-07-09 08:37:08 +0100

    Melhorias

---
+++ MVC/Controllers/ContaController.cs
@@ -0,0 +1,50 @@
+﻿using Dominio.Entidades;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserLogin login, string ReturnUrl)
+        {
+
+            while (ModelState.IsValid)
+            {
+                if (ValidarUsuario(login))
+                {
+                    FormsAuthentication.SetAuthCookie(login.Email, false);
+                    if (ReturnUrl != null)
+                    {
+                        return Redirect(ReturnUrl);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public ActionResult TerminarSessao()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/Conta/login");
+        }
+
+        private bool ValidarUsuario(UserLogin user)
+        {
+            //
+            return (user.Email == "teste" && user.Senha == "teste");
+        }
+    }
+}
---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Suporte()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Sobre()
+        {
+            return View("Index");
+        }
+    }
+}
