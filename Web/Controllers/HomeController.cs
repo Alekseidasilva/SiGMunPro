7bfa0b49de834998c7ae124b580dad0df5fb2138
Alekseidasilva <44441672+Alekseidasilva@users.noreply.github.com>
2020-10-10 07:38:41 +0100

    Ajuste nos controls

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,18 +1,31 @@
 ﻿using System.Web.Mvc;
+using Web.Models.Contratos.Repositorios;
 
 namespace Web.Controllers
 {
     [Authorize(Roles ="ADMINISTRADOR")]
     public class HomeController : Controller
     {
+        private readonly RepUsuario _usuario = new RepUsuario();
         [HttpGet]
         public ActionResult Dashboard()
-        {
+        { string nUser="";
+            if (_usuario.TotalUsuariosCadastrados()<10)
+            {
+                nUser = "0" + _usuario.TotalUsuariosCadastrados();
+                ViewBag.TotalUsuariosCadastrados = nUser;
+                return View();
+            }
+            ViewBag.TotalUsuariosCadastrados = _usuario.TotalUsuariosCadastrados();
             return View();
         }
 
         [HttpGet]
-        public ActionResult AnotherLink()
+        public ActionResult Suporte()
+        {
+            return View("Index");
+        }
+        public ActionResult Sobre()
         {
             return View("Index");
         }
---
+++ Web/Controllers/PerfilController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Web.Mvc;
+using Web.Helpers;
+using Web.Models.Contratos.Repositorios;
+using Web.Models.Entidades.Perfis;
+
+namespace Web.Controllers
+{
+     [Authorize(Roles = "ADMINISTRADOR")]
+    public class PerfilController : Controller
+    {
+        private readonly RepPerfil _perfil = new RepPerfil();
+        // GET: Perfil
+        public ActionResult Index()
+        {
+            var perfis = _perfil.SelecionarTodos();
+
+            return View(perfis);
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                Role roles = new Role
+                {
+                    Name = role.Name,
+                    DataCadastro = DateTime.Now.Date,
+                    Estado = role.Estado,
+                    IdCadastrador = GuardaSessao.Id
+
+                };
+                string res = _perfil.Cadastrar(roles);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        public ActionResult Alterar(int id)
+        {
+            var encontrado = _perfil.BuscarPorId(id);
+            return View(encontrado);
+        }
+        [HttpPost]
+        public ActionResult Alterar(Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                if (role.Id != 0)
+                {
+                    Role rolesRole = new Role
+                    {
+                        Id = role.Id,
+                        Name = role.Name,
+                        Estado = role.Estado
+                    };
+                    string res = _perfil.Alterar(rolesRole);
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+        public ActionResult Excluir(int id)
+        {
+            if (id != 0)
+            {
+                string res = _perfil.Excluir(id);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
