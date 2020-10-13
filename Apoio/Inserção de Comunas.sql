8d1ae3f3db87bde2fc32d9cff96d43a81c9df93d
Alekseidasilva <44441672+Alekseidasilva@users.noreply.github.com>
2020-10-13 18:21:56 +0100

    Scripts de Comunas
    script de inserção de comunas

---
+++ Apoio/Inserção de Comunas.sql
@@ -0,0 +1,161 @@
+﻿INSERT TB_Comunas (ComunaNome, ComunaMunicipioId)VALUES
+--***********PROVINCIA DO BENGO************************************
+---COMUNA DE AMBRIZ-----------------------
+ ('BELA VISTA', 1),('AMBRIZ', 1),('TABI', 1),
+---COMUNA DE BULA ATUMBA-----------------------  
+  ('BULA ATUMBA', 2),('QUIAGE', 2),
+-------COMUNA DE DANDE-----------------------
+('BARRA DO DANDE', 3),('CAXITO', 3),('MABUBAS', 3),('QUICABO', 3),('ÚCUA', 3), 
+-------COMUNA DE DEMBOS----------------------- 
+('QUIBAXE', 4),('PIN', 4),('PAREDES', 4),('COXE', 4),
+-------COMUNA DE NAMBUANGONGO----------------------- 
+  ('CANACASSALA', 5),('QUICUZO', 5),('MUXALUANDO', 5),('QUIXICO', 5),('GOMBE', 5),('CAGE', 5),('ZALA', 5),
+-------COMUNA DE PANGO ALUQUÉM-----------------------
+  ('PANGO ALÚQUÉM', 6),('CAZUANGONGO', 6), 
+--**************************************PROVINCIA DO BENGUELA************************************
+------------------COMUNA DE BALOMBO-------------------------------
+  ('BALOMBO', 7),('CHINGONGO', 7),('CHINDUMBO', 7),('MACAMOMBOLO', 7),
+------------------COMUNA DE BAÍA FARTA-------------------------------
+  ('DOMBE GRANDE',8 ),('BAIA FARTA', 8),('CALOHANGA', 8),('EQIPIMINA', 8),
+------------------COMUNA DE BENGUELA-------------------------------
+------------------COMUNA DE BOCOIO-------------------------------
+('BOCOIO', 10),('CUBALDO LUMBO', 10),('MONTE BELO', 10),('CHILA', 10),('PASSE', 10),
+------------------COMUNA DE CAIMBAMBO-------------------------------
+('CAIMBAMBO', 11),('CATENGUE', 11),('CAIAVE', 11),('CANHAMELA', 11),('VIANGOMBE', 11),
+------------------COMUNA DE CATUMBELA-------------------------------
+  ('CATUMBELA', 12),('BIÓPIO', 12),('GAMA', 12),('PRAIA BEBÉ', 12),
+------------------COMUNA DE CHONGORÓI-------------------------------
+  ('CHONGORÓI', 13),('BOLONGUERA', 13),('CAMUINE', 13),
+------------------COMUNA DE CUBAL-------------------------------
+  ('CAPUPA', 14),('CUBAL', 14),('TUMBULO', 14),('LAMBALA', 14),
+------------------COMUNA DE GANDA-------------------------------
+  ('GANDA', 15),('EBANGA', 15),('CHICUMA', 15),('BABAERA', 15),('CASSAQUE', 15),
+------------------COMUNA DE LOBITO-------------------------------
+  ('EGITO PRAIA',16),('LOBITO', 16),('CANATA', 16),('CANJALA', 16),
+ --**************************************PROVINCIA DO BIÉ************************************
+------------------COMUNA DE ANDULO-------------------------------
+  ------------------COMUNA DE CAMACUPA-------------------------------
+  ------------------COMUNA DE CATABOLA-------------------------------
+  ------------------COMUNA DE -------------------------------
+  ------------------COMUNA DE -------------------------------
+  ------------------COMUNA DE -------------------------------
+  ------------------COMUNA DE -------------------------------
+  ------------------COMUNA DE -------------------------------
+  ------------------COMUNA DE -------------------------------
+  
+
+
+
+
+
+
+
+
+
+--**************************************PROVINCIA DO MALANJE************************************
+------------------COMUNA DE CACUSO-------------------------------
+   ('CACUSO', 113),('LOMBE', 113),('QUIZENGA', 113),('PUNGU A NDONGO', 113),('SOQUECO', 113),
+  ------------------COMUNA DE CALANDULA-------------------------------
+  ('CALANDULA', 114),('CATECO CANGOLA', 114),('COTA', 114),('CUALE', 114),('QUINJE', 114),
+  ------------------COMUNA DE CAMBUNDI CATEMBO-------------------------------
+  ('TALAMUNGONGO', 115),('CAMBUNDI CATEMBO', 115),('QUITAPA', 115),('DUMBA CAMBANGO', 115),
+  ------------------COMUNA DE CANGANDALA-------------------------------
+   ('CANGANDALA', 116),('BEMBO', 116),('CULAMAGIA', 116),('CARIBO', 116),
+  ------------------COMUNA DE CAHOMBO-------------------------------
+  ('MBANJI YA NGOLA', 117),('CAHOMBO', 117),('CAMBO SUINGINGE', 117),('MICANDA', 117),
+  ------------------COMUNA DE CUABA N´ZOGE-------------------------------
+  ('KIWABA NZOJI',118 ),('MUFUMA', 118),
+  ------------------COMUNA DE CUNDA-DIA-BASE-------------------------------
+  ('KUNDA DYA BAZE', 119),('MILANDO', 119),('LEMBA', 119),
+  ------------------COMUNA DE LUQUEMBO-------------------------------
+  ('QUIMBANGO', 120),('CAPUNDA', 120),('DOMBO WA ZANGA', 120),('LUQUEMBO', 120),('CUNGA PALANGA', 120),('RIMBA', 120),
+  ------------------COMUNA DE MALANJE-------------------------------
+  ('MALANJE', 121),('NGOLA LUIJI', 121),('CAMBAXE', 121),
+  ------------------COMUNA DE MARIMBA-------------------------------
+  ('MARIMBA', 122),('CAMBOMBO', 122),('TEMBO ALUMA', 122),
+  ------------------COMUNA DE MASSANGO-------------------------------
+  ('MASSANGO', 123),('QUIHUHU', 123),('QUINGUENGUE', 123),
+  ------------------COMUNA DE MUCARI-------------------------------
+  ('CATALA', 124)
+  ,('CACULAMA', 124),('CAXINGA', 124),('MUQUIXE', 124),
+  ------------------COMUNA DE QUELA-------------------------------
+  ('QUELA', 125),('XANDEL', 125),('MOMA', 125),('BÂNGALAS', 125),
+  ------------------COMUNA DE QUIRIMA-------------------------------
+('SAUTAR', 126),('QUIRIMA', 126),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ('ANDULO', 17),('CALUCINGA', 17),('CASSUMBE', 17),('CHIVAÚLO', 17),('RINGOMA', 18),('CAMACUPA', 18),('MUINHA', 18),('UMPULO', 18),('CUANZA', 18),('CATABOLA', 19),('CHIPETA', 19),('CAIURA', 19),('CHIUCA', 19),('SANDE', 19),('CHINGUAR', 20)
+  ,('CUTATO', 20),('CANGOTE', 20),('CACHINGUES', 21),('CHITEMBO', 21),('MUTUMBO', 21),('MUMBUÉ', 21),('MALENGUE', 21),('SOMA CUANZA', 21),('LUANDO',22),('MUNHANGO', 22),('CEMBA', 22),('SACHINEMUNA', 22),('MUNHANGO', 22),('CUNHINGA', 23),('BELO HORIZONTE', 23)
+  ,('CUITO', 24),('CHICALA', 24),('CUNJE', 24),('TRUMBA', 24),('CAMBÂNDUA', 24),('NHARÊA', 25),('GAMBA', 25),('LÚBIA', 25),('CAIEI', 25),('DANDO', 25),
+  ,('MICONJE', 26),('BELIZE', 26),('LUALI', 26),('BUCO ZAU',27 ),('NECUTO', 27),('INHUCA', 27),('CABINDA', 28),('MALEMBO', 28),('TANDO ZINZE', 28),('CACONGO', 29),('DINGE', 29),('MASSABI', 29),('MAUÉ',30),('CALAI', 30),('MAVENGUE', 30)
+  ,('SAVATE', 31),('CAILA',31 ),('CUANGAR', 31),('CUCHI', 32),('CUTATO', 32),('CHINGUANJA', 32),('VISSATI', 32),('LUPIRE', 33),('CUITO CUANAVALE', 33),('LONGA', 33),('BAIXO LONGA', 33),('DIRICO', 34),('MUCUSSO', 34),('XAMAVERA', 34),('',3 )
+  ,('MAVINGA', 36),('CUTUILE', 36),('CUNJUMBA', 36),('LUENGUE', 36),('CAIUNDO',37),('MENONGUE', 37),('MISSOMBO', 37),('CUEIO', 37),('RITO', 38),('NANCOVA', 38),('RIVUNGO', 39),('XIPUNDO', 39)
+  ,('LUIANA', 39),('CAHAMA',40),('OTCHINJAU', 40),('ONDJIVA', 41),('NEHONE CAFIMA', 41),('EVALE', 41),('TCHOMPOLO OXIMOLO', 41),('MÔNGWA', 41),('ONCÓCUA',42 ),('CHITADO',42),('MUPA', 43),('MUKOLONGODJO', 43),('CALONGA', 43),('CUBATI', 43),('NAMACUNDE', 44)
+  ,('CHIEDE', 44),('HUMPE', 45),('MUCOPE', 45),('NAULILA', 45),('OMBALA YO MUNGU', 45),('XANGONGO', 45),('BIMBE', 46),('BAILUNDO', 46),('LUNGE', 46),('LUVEMBA', 46),('HENGUE', 46),('CHIUMBO', 47),('CHINHAMA',47),('CACHIUNGO',47),('CAÁLA', 48)
+  ,('CATATA', 48),('CALENGA', 48),('CUIMA', 48),('QUIPEIO', 49),('ECUNHA', 49),('HUAMBO', 50),('CALIMA', 50),('CHIPIPA', 50),('LONDUIMBALI', 51),('CUMBIRA', 51),('GALANGA', 51),('USSOQUE', 51),('ALTO HAMA', 51),('LÉPI', 52),('LAVA', 52)
+  ,('CHILATA', 52),('LONGONJO', 52),('MUNGO', 53),('CAMBUENGO', 53),('SAMBO', 54),('MBAWE', 54),('CHICALA', 54),('SAMBOLO', 54),('CHINJENJE', 55),('CHIACA', 55),('UCUMA', 56),('CACOMA', 56),('MUNDUNDO', 56),('CANCONDA', 57),('GUNGUE', 57)
+  ,('UABA', 57),('CUSSE', 57),('CHITUTO', 58),('VITI VIVALI', 58),('CACULA', 58),('TCHICUAQUEIA', 58),('CALUQUEMBE', 59),('CALEPI', 59),('NGOLA', 59),('CHIBIA', 61),('JAU', 61),('CAPUNDA', 61),('CAVILONGO', 61),('QUIHITA', 61),('CHICOMBA',62)
+  ,('CUTENDA', 62),('BAMBI', 63),('CHIPINDO', 63),('CUVANGO', 64),('GALANGUE',64),('VICUNGO', 64),('DONGOI', 66),('CASSINGA', 66),('JAMBA', 66),('LUBANGO', 67),('HOQUE', 67),('ARIMBA', 67),('HUÍLA', 67),('CAPELONGO', 68),('MATALA', 68)
+  ,('MULONGO', 68),('QUILENGUES', 69),('IMPULOS', 69),('DINDE', 69),('CAMABATELA',71),('TANGO', 71),('MÁUA', 71),('BINDO', 71),('LUINGA', 71),('BANGA', 72),('CACULO CABAÇA', 72),('ALDEIA NOVA', 72),('CARIAMBA', 72),('BOLONGONGO', 63),('TERREIRO', 63)
+  ,('QUIQUIEMBA',73),('DONDO', 74),('MASSANGANO', 74),('DANGE YA MENHA', 74),('ZENZA DO ITOMBE', 74),('SÃO PEDRO DA QUILEMBA', 74),('NDALATANDO', 75),('CANHOCA', 75),('GOLUNGO ALTO', 76),('CAMBONDO', 76),('CÊRCA', 76),('QUILUANJE', 76),('QUILOMBO DOS DEMBOS', 77),('CAMAME', 77),('CAVUNGA', 77)
+  ,('LUCALA', 78),('QUIANGOMBE', 78),('SAMBA CAJÚ', 80),('SAMBA LUCALA', 80),('GABELA', 81),('ASSANGO', 81),('CASSONGUE', 82),('PAMBANGALA', 82),('DUMBI', 82),('ATÓME',82),('SANGA', 83),('WAKU KUNGO', 83),('QUISSANGA CUNJO', 83),('CONDA', 84),('CUNJO', 84)
+  ,('EBO', 85),('CONDÉ', 85),('QUISSANJE', 85),('CALULO', 86),('MUNENGA', 86),('CABUTA', 86),('QUISSONGO', 86),('MUSSENDE', 87),('QUIENHA', 87),('SÃO LUCAS', 87),('CAPOLO', 88),('PORTO AMBOIM', 88),('QUIBALA', 89),('DALA CACHIBO', 89),('CARIANGO', 89)
+  ,('LONHE', 89),('QUILENDA',90),('QUIRIMBO', 90),('SELES', 91),('AMBOIVA', 91),('BOTERA', 91),('SUMBE', 92),('GUNGO', 92),('GANGULA', 92),('QUICOMBO', 92),('BARRA DO KWANZA', 93),('FUNDA', 94),('CASSONECA',96),('CABIRI', 96)
+  ,('BOM JESUS', 96),('CACULO CAHANGO', 96),('QUIMINHA', 96),('MUXIMA', 98),('DEMBA CHIO', 98),('QUIXINGE', 98),('MUMBONDO', 98),('CABO LEDO', 98),('CALUMBO', 99),('LUIA', 100),('CACHIMO', 100),('CANZAR', 100),('CAMBULO', 100),('CAPENDA-CAMULEMBA', 101),('XINGE', 101)
+  ,('CAMAXILO', 102),('CAUNGILA', 102),('CHITATO', 103),('LUACHIMO', 103),('CUANGO', 104),('LOREMO', 104),('CUÍLO', 105),('CALUANGO', 105),('LUBALO', 106),('LUANGUE', 106),('MUVULUEGE', 106),('LUCAPA', 107),('CAPAIA', 107),('CAMISSOMBO', 107),('XÁ CASSAU', 107)
+  ,('XÁ MUTEBA', 108),('LONGO', 108),('CASSANJE CALUCAA', 108),('XASSENGUE',109),('CUCUMBI', 109),('ALTO CHICAPA',109),('CACOLO', 109),('DALA', 110),('LUMA CASSAI', 110),('CAZAGE', 110),('CHILUAGE', 111),('CASSAI', 111),('MURIEGE', 111),('MUCONDA', 111),('DAURIMO', 112)
+  ,('MONA QUIMBUNDO', 112),('SOMBO', 112),
+  
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ('NANA CANDUNDO', 127),('LUMBALA CAQUENGUE', 127),('CAZOMBO', 127),('MACONDO', 127),('CAIANDA', 127),('CALUNDA', 127)
+  ,('LÓVUA', 127),('LUTEMBO', 128),('CHIUME', 128),('LUMBALA NGUIMBO', 128),('LUVURI', 128),('NINDA', 128),('MUSSUMA', 128),('SESSA', 128),('LUACANO', 128),('LAGO DILOLO', 128),('LÉUA', 130),('LIANGONGO', 130),('LUACANO', 132),('LAGO DILOLO', 132),('CANGOMBE',133 )
+  ,('CASSAMBA', 133),('TEMPUÉ', 133),('CANGAMBA', 133),('MUIÉ', 133),('LUCUSSE', 136),('CACHIPOQUE', 136),('LUENA', 136),('MUANGAI', 136),('BIBALA', 137),('CAITOU', 137),('CAPANGOMBE', 137),('LOLA', 137),('CAMUCULO', 138),('MAMUÉ', 138),('CHINGO', 138)
+  ,('MOCÂMEDES', 139),('LUCIRA', 139),('BENTIABA', 139),('FORTE SANTA RITA', 139),('BAIA DO TIGRE', 140),('LONA', 140),('TÔMBWA', 140),('VIREI', 141),('CAINDE', 141),('CANGOLA',142 ),('BENGO', 142),('CAIONGO', 142),('NOVA AMBUILA', 143),('QUIPEDRO', 143),('BEMBE', 144)
+  ,('LUCUNGA', 144),('MABAIA', 144),('BUENGAS SUL', 145),('NOVA ESPERANÇA', 145),('CUILO CAMBOSO', 145),('DAMBA', 147),('NSOSSO', 147),('CAMATAMBO', 147),('LÊMBOA', 147),('PETECUSSO', 147),('MACOCOLA', 149),('MILUNGA', 149),('MACOLO', 149),('MASSAU', 149),('MUCABA', 150)
+  ,('UANDO MUCABA', 150),('DIMUCA', 151),('QUISSEQUE', 151),('NEGAGE', 151),('CUANGO', 153),('ICOCA', 153),('QUIMBELE', 153),('ALTO ZAZA', 153),('QUITEXE', 154),('ALDEIA VIÇOSA', 154),('CAMBAMBA', 154),('VISTA ALEGRE', 154),('SANZA POMBO', 155),('CUILO POMBO', 155),('UAMBA', 155)
+  ,('ALFÂNDEGA', 155),('SONGO', 156),('QUIVUENGA', 156),('CUILO FUTA',158),('MAQUELA ZOMBBO', 158),('QUIBOCOLO', 158),('BÉU', 158),('SACANDICA', 158),('CUIMBA', 159),('BUELA', 159),('SERRA DA CANDA', 159),('LUVACA', 159),('MBANZA KONGO', 160),('LUVO', 160),('CALUCA', 160)
+  ,('MADIMBA', 160),('QUIENDE', 160),('CALAMBALA', 160),('NÓQUI', 161),('LUFICO', 161),('MPALA',161),('NZETO', 162),('QUINDEJE', 162),('MUSSERRA', 162),('QUIBALA NORTE', 162),('SOYO', 163),('SUMBA', 163),('PEDRA DE FEITIÇO', 163),('QUÊLO', 163),('MANGUE GRANDE', 163)
+  ,('TOMBOCO',164),('QUISIMBA', 164),('QUINZAU', 164)
+
+
+
+          
